8c3557919b76651ea394db3ecd7f0d128f5ab96f
nanaisisi <131863615+nanaisisi@users.noreply.github.com>
2026-09-08 21:39:47 +0900

    refactor: add conditional compilation for Windows subsystem in main function

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use ui::app::RammapApp;
 use ui::types::AppInput;
 use windows_reactor::App;
 
+#[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     App::run_component::<RammapApp>(AppInput)?;
     Ok(())
